e374cf3d293fdc15a29256048f193f47c8d531e9
SantiagoBecerra21 <santibecerraortiz@gmail.com>
2022-11-11 11:34:42 -0500

    Ajustes al script

--- Scripts/Script Limpieza.R
+++ Scripts/Script Limpieza.R
@@ -1,3 +1,12 @@
+################################################
+#                 Taller 2                     #
+#                 LIMPIEZA                     #
+# Colaboradores                                #
+# Daniel Raquira - 201914059                   #
+# Santiago Becerra - 201911587                 #
+################################################
+
+
 ### Primero revisamos los datos y los limpiamos ###
 rm(list=ls())
 require(pacman) 
@@ -16,12 +25,12 @@ test <- readRDS("test.RDS")
 
 train <- train %>%
   mutate_at(.vars = c(
-    "property_type","operation_type"),
+    "property_type","operation_type", "city"),
     .funs = factor)
 
 test <- test %>%
   mutate_at(.vars = c(
-    "property_type","operation_type"),
+    "property_type","operation_type", "city"),
     .funs = factor)
 
 ## Datos espaciales ##
@@ -30,4 +39,43 @@ train<-st_as_sf(train,coords=c('longb','latb'),crs="WGS84") #crs utilizado por g
 test<-test %>% mutate(latb=lat,longb=lon)
 test<-st_as_sf(test,coords=c('longb','latb'),crs="WGS84")
 
+##Revisamos la variable city que e sla que nos interesa ##
+train %>%
+  group_by(city) %>%
+  summarise(n = n()) %>%
+  mutate(
+    totalN = (cumsum(n)),
+    percent = round((n / sum(n)), 3),
+    cumuPer = round(cumsum(freq = n / sum(n)), 3)) 
+
+test %>%
+  group_by(city) %>%
+  summarise(n = n()) %>%
+  mutate(
+    totalN = (cumsum(n)),
+    percent = round((n / sum(n)), 3),
+    cumuPer = round(cumsum(freq = n / sum(n)), 3)) 
+
+## Separamos de la base train Bogotá y Medellín ##
+train_bog<-subset(train,city %in% c("Bogotá D.C"))
+train_med<-subset(train,city %in% c("Medellín"))
+
+## Empezamos con Bogotá ##
+point = geocode_OSM(paste(train$lat[1]," , ",train$lon[1]), as.sf=T) 
+leaflet() %>% addTiles() %>% addCircles(data=point)
+leaflet() %>% addTiles() %>% addCircles(data=train_bog)
+
+##Supermercados##
+osm = opq(bbox = getbb("Bogot? Colombia")) %>%
+  add_osm_feature(key="amenity" , value="marketplace") 
+class(osm)
+
+## extraer Simple Features Collection
+osm_sf = osm  %>% osmdata_sf()
+osm_sf
+
+marketplace = osm_sf$osm_points %>% select(osm_id,amenity) 
+marketplace
+
+leaflet() %>% addTiles() %>% addCircleMarkers(data=marketplace , col="blue")
 
