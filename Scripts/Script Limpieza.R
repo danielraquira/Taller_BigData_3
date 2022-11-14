b07899a985fc46fcb087c123281c4524b0670d72
SantiagoBecerra21 <santibecerraortiz@gmail.com>
2022-11-14 00:08:29 -0500

    Limpieza y predicciones train

--- Scripts/Script Limpieza.R
+++ Scripts/Script Limpieza.R
@@ -19,6 +19,7 @@ p_load(tidyverse,rio,
        secr,
        osmdata,
        here) 
+path = here('')
 setwd("C:/Users/Santiago Becerra/Desktop/Santiago/Andes/Materias y Trabajos/Octavo Semestre/Big Data/Problem set 3/Taller_BigData_3/Stores")
 train <- readRDS("train.Rds")
 test <- readRDS("test.RDS")
@@ -105,9 +106,9 @@ leaflet() %>% addTiles() %>% addCircleMarkers(data=estaciones_de_bus , col="red"
 ### Distancias buses ###
 distancias_bus = st_distance(x=train_bog , y=estaciones_de_bus)
 
-min_distancias_bus = apply(distancias_bus , 1 , min)
+min_dist_bus = apply(distancias_bus , 1 , min)
 
-train_bog$min_distancias_bus<-min_distancias_bus
+train_bog$min_dist_bus<-min_dist_bus
 
 
 leaflet() %>% addTiles() %>% 
@@ -130,14 +131,14 @@ marketplace
 
 leaflet() %>% addTiles() %>% addCircleMarkers(data=marketplace , col="blue")
 
-### distanciaa supermercados ###
+### distancia supermercados ###
 distancias_market = st_distance(x=train_med, y=marketplace)
 distancias_market
 
-min_distancias_market = apply(distancias_market , 1 , min)
-min_distancias_market
+min_dist_market = apply(distancias_market , 1 , min)
+min_dist_market
 
-train_med$min_distancias_market<-min_distancias_market
+train_med$min_dist_market<-min_dist_market
 
 leaflet() %>% addTiles() %>% 
   addCircles(data=marketplace , col="black" , weight=2)  
@@ -161,23 +162,229 @@ leaflet() %>% addTiles() %>% addCircleMarkers(data=estaciones_de_busM , col="red
 #### distancia buses ####
 distancia_bus = st_distance(x=train_med , y=estaciones_de_busM)
 
-min_distancia_bus = apply(distancia_bus , 1 , min)
+min_dist_bus = apply(distancia_bus , 1 , min)
 
-train_med$min_distancia_bus<-min_distancia_bus
+train_med$min_dist_bus<-min_dist_bus
 
 
 leaflet() %>% addTiles() %>% 
   addCircles(data=train_med , col="red" , weight=2) %>% 
   addCircles(data=estaciones_de_busM , col="black" , weight=2)
 
-## Creación test conjunta
-
-test_final<-rbind(test_bog,test_med) ### revision
+train_final<-rbind(train_bog,train_med) 
 
 
-##### Predicción en areas según vecinos cercanos
-path = here('')
-## Primero Bogota
+## Hacemos lo mismo para Cali que es el test ##
 
-mzbog<-st_read(here(path,"11_BOGOTA/URBANO/MGN_URB_MANZANA.shp"))
-######## este es el que no me sale
+point = geocode_OSM(paste(test$lat[1]," , ",test$lon[1]), as.sf=T) 
+leaflet() %>% addTiles() %>% addCircles(data=point)
+leaflet() %>% addTiles() %>% addCircles(data=test)
+
+## objeto osm marketplace
+osm = opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key="amenity" , value="marketplace") 
+class(osm)
+
+## extraer Simple Features Collection
+osm_sf = osm %>% osmdata_sf()
+osm_sf
+
+marketplace = osm_sf$osm_points %>% select(osm_id,amenity) 
+marketplace
+
+leaflet() %>% addTiles() %>% addCircleMarkers(data=marketplace , col="blue")
+
+##distancia marketplace
+dist_market = st_distance(x=test , y=marketplace)
+dist_market
+min_dist_market = apply(dist_market , 1 , min)
+min_dist_market
+test$min_dist_market<-min_dist_market
+
+leaflet() %>% addTiles() %>% 
+  addCircles(data=marketplace , col="black" , weight=2)
+
+
+## objeto osm buses
+osm = opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key="amenity" , value="bus_station") 
+class(osm)
+
+## extraer Simple Features Collection
+osm_sf = osm %>% osmdata_sf()
+osm_sf
+
+estaciones_busC = osm_sf$osm_points %>% select(osm_id,amenity) 
+estaciones_busC
+
+leaflet() %>% addTiles() %>% addCircleMarkers(data=estaciones_busC , col="red") %>% 
+  addCircleMarkers(data=test, col="blue")
+
+
+## distancia buses
+dist_bus = st_distance(x=test , y=estaciones_busC)
+
+min_dist_bus = apply(dist_bus , 1 , min)
+
+test$min_dist_bus<-min_dist_bus
+
+
+leaflet() %>% addTiles() %>% 
+  addCircles(data=point , col="red" , weight=2) %>% 
+  addCircles(data=estaciones_busC , col="black" , weight=2) 
+
+
+## Hacemos predicciones para Bogotá ##
+
+#Predicción a partir de descripción
+table(is.na(train_bog$surface_total))
+table(is.na(train_bog$surface_covered))
+
+train_bog = train_bog %>% mutate(new_surface=surface_total)
+train_bog = train_bog %>% mutate(new_surface=ifelse(is.na(new_surface)==T,surface_covered,new_surface))
+
+table(is.na(train_bog$new_surface))
+
+train_bog = train_bog %>% mutate(new_desc=str_to_lower(description))
+
+p1 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m" ## pattern
+p2 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m2" ## pattern
+
+train_bog$new_surface2<-NA
+
+train_bog = train_bog %>% 
+  mutate(new_surface2 = str_extract(string=train_bog$new_desc , pattern= p1))
+train_bog = train_bog %>% 
+  mutate(new_surface2 = ifelse(is.na(new_surface2)==T,
+                               str_extract(string=train_bog$new_desc , pattern= p2),
+                               new_surface2))
+train_bog$new_surface3<-NA
+train_bog = train_bog %>% 
+  mutate(new_surface3 = str_extract(string=train_bog$new_surface2 , pattern= "[:digit:]{2,3}"))
+train_bog$new_surface3 <- as.numeric(train_bog$new_surface3)
+train_bog = train_bog %>% mutate(new_surface3 = ifelse(new_surface3<=40,NA,new_surface3))
+
+train_bog = train_bog %>% mutate(new_surface = ifelse(is.na(new_surface)==T,new_surface3,new_surface))
+table(is.na(train_bog$new_surface))
+
+# buffer
+house_buf<-NULL
+house_buf_mean<-NULL
+house_buf = st_buffer(train_bog,dist=100)
+house_buf = st_join(house_buf,train_bog[,"new_surface"])
+st_geometry(house_buf) = NULL
+house_buf_mean = house_buf %>% group_by(property_id) %>% summarise(surface_new_3=mean(new_surface.y,na.rm=T))
+train_bog = left_join(train_bog,house_buf_mean,"property_id")
+
+train_bog = train_bog %>% mutate(new_surface = ifelse(is.na(new_surface)==T,train_bog$surface_new_3,new_surface))
+table(is.na(train_bog$new_surface))
+
+train_bog = train_bog %>% mutate(new_surface = ifelse(is.na(new_surface)==T,mean(train_bog[is.na(train_bog$new_surface)==F,]$new_surface),new_surface))
+table(is.na(train_bog$new_surface))
+table(is.na(train_bog$property_type))
+
+
+##Prediccion si tiene balcon / terraza
+p1 = "balcon" ## pattern
+p2 = "balcon" ## pattern
+p3 = "terraza" ## pattern
+
+train_bog$balcon<-NULL
+
+train_bog = train_bog %>% 
+  mutate(balcon = str_extract(string=train_bog$new_desc , pattern= p1))
+train_bog = train_bog %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=train_bog$new_desc , pattern= p2),balcon))
+train_bog = train_bog %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=train_bog$new_desc , pattern= p3),balcon))
+
+train_bog = train_bog %>% mutate(balcon_terr = ifelse(is.na(balcon)==T, 0 ,1))
+
+
+train_bog <- train_bog %>%
+  mutate_at(.vars = c(
+    "balcon_terr"),
+    .funs = factor)
+
+## Hacemos predicciones para Medellín ##
+
+#Predicción a partir de descripción
+table(is.na(train_med$surface_total))
+table(is.na(train_med$surface_covered))
+train_med$new_surface<-NULL
+train_med = train_med %>% mutate(new_surface=surface_total)
+table(is.na(train_med$new_surface))
+train_med = train_med %>% mutate(new_surface=ifelse(is.na(new_surface)==T,surface_covered,new_surface))
+table(is.na(train_med$new_surface))
+
+
+train_med = train_med %>% mutate(new_desc=str_to_lower(description))
+
+
+p1 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m" 
+p2 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m2"
+
+train_med$new_surface2<-NA
+
+train_med = train_med %>% 
+  mutate(new_surface2 = str_extract(string=train_med$new_desc , pattern= p1))
+train_med = train_med %>% 
+  mutate(new_surface2 = ifelse(is.na(new_surface2)==T,
+                               str_extract(string=train_med$new_desc , pattern= p2),
+                               new_surface2))
+train_med$new_surface3<-NA
+
+train_med = train_med %>% 
+  mutate(new_surface3 = str_extract(string=train_med$new_surface2 , pattern= "[:digit:]{2,3}"))
+train_med$new_surface3 <- as.numeric(train_med$new_surface3)
+train_med = train_med %>% mutate(new_surface3 = ifelse(new_surface3<=40,NA,new_surface3))
+
+train_med = train_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,new_surface3,new_surface))
+table(is.na(train_med$new_surface))
+
+
+# buffer
+house_buf<-NULL
+house_buf_mean<-NULL
+house_buf = st_buffer(train_med,dist=100)
+house_buf = st_join(house_buf,train_med[,"new_surface"])
+st_geometry(house_buf) = NULL
+house_buf_mean = house_buf %>% group_by(property_id) %>% summarise(surface_new_3=mean(new_surface.y,na.rm=T))
+train_med = left_join(train_med,house_buf_mean,"property_id")
+
+train_med = train_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,train_med$surface_new_3,new_surface))
+table(is.na(train_med$new_surface))
+
+train_med = train_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,mean(train_med[is.na(train_med$new_surface)==F,]$new_surface),new_surface))
+table(is.na(train_med$new_surface))
+table(is.na(train_med$property_type))
+
+
+##Prediccion si tiene balcon / terraza
+p1 = "balcon" ## pattern
+p2 = "balcon" ## pattern
+p3 = "terraza" ## pattern
+
+train_med$balcon<-NULL
+
+train_med = train_med %>% 
+  mutate(balcon = str_extract(string=train_med$new_desc , pattern= p1))
+train_med = train_med %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=train_med$new_desc , pattern= p2),balcon))
+train_med = train_med %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=train_med$new_desc , pattern= p3),balcon))
+
+train_med = train_med %>% mutate(balcon_terr = ifelse(is.na(balcon)==T, 0 ,1))
+
+
+train_med <- train_med %>%
+  mutate_at(.vars = c(
+    "balcon_terr"),
+    .funs = factor)
+
+## Predicción para Cali / test ##
+
